a34aeba8a60efeee0fb355018e194dfbc473364c
IshanKashyap03 <ikashyap@ualberta.ca>
2022-08-23 13:20:04 -0600

    Simon Game

---
+++ Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,72 @@
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+function nextSequence() {
+  $("h1").text("Level " + level);
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
+  level++;
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {
+  $("#" + currentColor).addClass("pressed");
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    if (userClickedPattern.length == gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+      userClickedPattern = [];
+    }
+  } else {
+    var audio = new Audio("sounds/wrong.mp3");
+    audio.play();
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    $("h1").text("Game Over, Press Any Key to Restart");
+    startOver();
+  }
+}
+
+function startOver() {
+  level = 0;
+  userClickedPattern = [];
+  gamePattern = [];
+  started = false;
+}
+
+$(".btn").click(function () {
+  var userChosenColor = $(this).attr("id");
+  userClickedPattern.push(userChosenColor);
+  animatePress(userChosenColor);
+  playSound(userChosenColor);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+$(document).keydown(function () {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
